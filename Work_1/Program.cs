3efbe828510f4178c841c08d9fed8d7ac516ddb3
NikitaMeleckhin <160129154+NikitaMeleckhin@users.noreply.github.com>
2024-09-11 09:00:58 +0300

    Add files via upload

---
+++ Work_1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Work_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Выполнение операции над числами");
+            Console.Write("Введите первое число: ");
+            double a = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Введите второе число: ");
+            double b = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Сумма чисел равна " + (a + b));
+            Console.WriteLine("Для выхода нажмите любую клавишу");
+            Console.ReadKey();
+        }
+    }
+}
